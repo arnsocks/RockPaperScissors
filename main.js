38dc0628184976dc23e9e21f5dbaabe9fe4a481a
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-08-13 18:09:38 -0400

    Create the playGame function

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-console.log("Hello from the main.js");
-
-function getComputerChoice() {
-  // CREATE a variable call Choice
-  // CREATE a random number between 0 and 2
-  // if result is 0 choice is "Rock"
-  // if result is 1 choice is "Paper"
-  // if result is 2 choice is "Scissors"
-  // RETURN choice
-
+function getComputerChoice() { // Have the computer randomly select a choice
   let choice = '';
   let num = Math.floor(Math.random()*3);
   if (num == 0) {
@@ -19,19 +10,12 @@ function getComputerChoice() {
   } else {
     choice = "Something went wrong";
   }
-
   return choice;
 }
 
-function getHumanChoice() {
-  // CREATE a variable called choice
-  // PROMPT the player for their choice
-  // RETURN the choice
-
+function getHumanChoice() { // Ask the human for their choice
   let choice = '';
   let input = prompt("Rock, Paper, or Scissors?", '');
-
-  // TO DO: validate the input
   input = input.toLowerCase();
   if (input == 'rock') {
     choice = "Rock";
@@ -45,39 +29,44 @@ function getHumanChoice() {
   return choice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame(numRounds) {
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playRound(humanChoice, computerChoice){
-  function determineWinner(humanPick, computerPick) {
-    if (humanPick === computerPick) {
-      return 'draw';
-    } else if (humanPick === 'Rock') {
-      return (computerPick === 'Scissors') ? 'human' : 'computer';
-    } else if (humanPick === 'Paper') {
-      return (computerPick === 'Rock') ? 'human' : 'computer';
-    } else if (humanPick === 'Scissors') {
-      return (computerPick === 'Paper') ? 'human' : 'computer';
-    } 
-
+  for (let i = 0; i < numRounds; i++) {
+      playRound();
   }
 
-  let winner = determineWinner(humanChoice,computerChoice);
+  function playRound(){
+    
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    let winner = determineWinner(humanChoice, computerChoice);
 
-  if (winner == 'human') {
-    humanScore ++;
-    console.log("You won the round");
-  } else if(winner == 'computer') {
-    computerScore ++;
-    console.log("You lost the round.");
-  } else {
-    console.log("The round was a draw.");
+    if (winner == 'human') {
+      humanScore ++;
+      console.log("You won the round");
+    } else if(winner == 'computer') {
+      computerScore ++;
+      console.log("You lost the round.");
+    } else {
+      console.log("The round was a draw.");
+    }
+    console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}`);
+    console.log(`Computer: ${computerScore} | Human: ${humanScore}`);
+
+    function determineWinner(humanPick, computerPick) { //Figure out the result of the round
+      if (humanPick === computerPick) { // if the picks are the same then it's always a draw
+        return 'draw';
+      } else if (humanPick === 'Rock') {
+        return (computerPick === 'Scissors') ? 'human' : 'computer';
+      } else if (humanPick === 'Paper') {
+        return (computerPick === 'Rock') ? 'human' : 'computer';
+      } else if (humanPick === 'Scissors') {
+        return (computerPick === 'Paper') ? 'human' : 'computer';
+      } 
+    }
   }
-  console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}`);
-  console.log(`Computer: ${computerScore} | Human: ${humanScore}`);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame(5);
