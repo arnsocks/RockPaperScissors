d282219909726ebc68c8efaf085248bce787a2c5
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-08-28 15:25:10 -0400

    Implement buttons to input player choice

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const buttons = document.querySelectorAll('button');
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() { // Have the computer randomly select a choice
   let choice = '';
   let num = Math.floor(Math.random()*3);
@@ -13,60 +18,36 @@ function getComputerChoice() { // Have the computer randomly select a choice
   return choice;
 }
 
-function getHumanChoice() { // Ask the human for their choice
-  let choice = '';
-  let input = prompt("Rock, Paper, or Scissors?", '');
-  input = input.toLowerCase();
-  if (input == 'rock') {
-    choice = "Rock";
-  } else if (input == 'paper') {
-    choice = "Paper";
-  } else if (input == 'scissors') {
-    choice = "Scissors";
+for (button of buttons) {
+  button.addEventListener('click', playRound);
+}
+
+function playRound(){
+  const humanChoice = this.id;
+  const computerChoice = getComputerChoice();
+  let winner = determineWinner(humanChoice, computerChoice);
+
+  if (winner == 'human') {
+    humanScore ++;
+    console.log("You won the round");
+  } else if(winner == 'computer') {
+    computerScore ++;
+    console.log("You lost the round.");
   } else {
-    choice = "Invalid choice";
+    console.log("The round was a draw.");
   }
-  return choice;
-}
+  console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}`);
+  console.log(`Computer: ${computerScore} | Human: ${humanScore}`);
 
-function playGame(numRounds) {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < numRounds; i++) {
-      playRound();
-  }
-
-  function playRound(){
-    
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    let winner = determineWinner(humanChoice, computerChoice);
-
-    if (winner == 'human') {
-      humanScore ++;
-      console.log("You won the round");
-    } else if(winner == 'computer') {
-      computerScore ++;
-      console.log("You lost the round.");
-    } else {
-      console.log("The round was a draw.");
-    }
-    console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}`);
-    console.log(`Computer: ${computerScore} | Human: ${humanScore}`);
-
-    function determineWinner(humanPick, computerPick) { //Figure out the result of the round
-      if (humanPick === computerPick) { // if the picks are the same then it's always a draw
-        return 'draw';
-      } else if (humanPick === 'Rock') {
-        return (computerPick === 'Scissors') ? 'human' : 'computer'; // Rock beats Scissors
-      } else if (humanPick === 'Paper') {
-        return (computerPick === 'Rock') ? 'human' : 'computer'; // Paper beats Rock
-      } else if (humanPick === 'Scissors') {
-        return (computerPick === 'Paper') ? 'human' : 'computer'; // Scissors beats Paper
-      } 
-    }
+  function determineWinner(humanPick, computerPick) { //Figure out the result of the round
+    if (humanPick === computerPick) { // if the picks are the same then it's always a draw
+      return 'draw';
+    } else if (humanPick === 'Rock') {
+      return (computerPick === 'Scissors') ? 'human' : 'computer'; // Rock beats Scissors
+    } else if (humanPick === 'Paper') {
+      return (computerPick === 'Rock') ? 'human' : 'computer'; // Paper beats Rock
+    } else if (humanPick === 'Scissors') {
+      return (computerPick === 'Paper') ? 'human' : 'computer'; // Scissors beats Paper
+    } 
   }
 }
-
-playGame(5);
